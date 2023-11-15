3cdb23af3cbc99de53ce42a234d427f510bf98c0
Andreas Håvardsen <andreas.havardsen@autostoresystem.com>
2023-11-15 17:31:22 +0100

    Add example project

---
+++ exampleProj/build/CMakeFiles/main.x.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.x.dir/main.cpp.obj"
+  "CMakeFiles/main.x.dir/main.cpp.obj.d"
+  "libmain.x.dll.a"
+  "main.x.exe"
+  "main.x.exe.manifest"
+  "main.x.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.x.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ exampleProj/build/CMakeFiles/main.x.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for main.x.
+# This may be replaced when dependencies are built.
